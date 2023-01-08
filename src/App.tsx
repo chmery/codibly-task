f0d7fe78045dc70b7de7129208e8bf07383d8ff2
chmery <karektv@gmail.com>
2023-01-08 16:43:49 +0100

    Create loader component

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import styles from "./App.module.css";
 import Input from "./components/Input/Input";
 import List from "./components/List/List";
 import Pagination from "./components/Pagination/Pagination";
+import Loader from "./components/UI/Loader/Loader";
 import Wrapper from "./components/Wrapper/Wrapper";
 import useFetchProductsData from "./hooks/useFetchProductsData";
 
@@ -10,9 +11,16 @@ const App = () => {
 
     return (
         <Wrapper>
-            <Input />
-            {productsData && <List productsData={productsData.data} />}
-            {productsData && <Pagination totalPages={productsData.totalPages} />}
+            <div className={styles.app}>
+                {!productsData && <Loader />}
+                {productsData && (
+                    <>
+                        <Input />
+                        <List productsData={productsData.data} />
+                        <Pagination totalPages={productsData.totalPages} />
+                    </>
+                )}
+            </div>
         </Wrapper>
     );
 };
