2acc90c75a543c6a6b2c7f51c21f3b2a311970bb
chmery <karektv@gmail.com>
2023-01-12 11:52:19 +0100

    Create tests for list item component

---
+++ src/components/List/ListItem/ListItem.test.tsx
@@ -0,0 +1,44 @@
+import { render, screen, fireEvent } from "@testing-library/react";
+import ListItem from "./ListItem";
+import { Provider } from "react-redux";
+import store from "../../../store/store";
+
+const productData = {
+    id: 1,
+    name: "cerulean",
+    year: 2000,
+    color: "#98B2D1",
+    pantoneValue: "15-4020",
+};
+
+const { id, name, year } = productData;
+
+describe("List item component", () => {
+    it("should render the product data", () => {
+        render(
+            <Provider store={store}>
+                <ListItem productData={productData} />
+            </Provider>
+        );
+
+        const productId = screen.getByText(id);
+        const productName = screen.getByText(name);
+        const productYear = screen.getByText(year);
+
+        expect(productId && productName && productYear).toBeVisible();
+    });
+
+    it("should pass product data to modal when clicked on", () => {
+        render(
+            <Provider store={store}>
+                <ListItem productData={productData} />
+            </Provider>
+        );
+
+        fireEvent.click(screen.getByText(name));
+
+        const { modal } = store.getState();
+
+        expect(modal.dataModal.productData?.name).toBe(name);
+    });
+});
