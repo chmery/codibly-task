5eb46ae15517f0a0967b73b09b9e373b976136e0
chmery <karektv@gmail.com>
2023-01-14 15:17:11 +0100

    Update tests for app component

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -14,61 +14,72 @@ const MockApp = ({ path }: { path: string }) => {
     );
 };
 
+const mockFetch = (ok: boolean, status: number, data: any) => {
+    jest.spyOn(global, "fetch").mockImplementation(
+        jest.fn(() =>
+            Promise.resolve({
+                ok,
+                status,
+                json: () => Promise.resolve(data),
+            })
+        ) as jest.Mock
+    );
+};
+
+const productData = {
+    data: {
+        id: 2,
+        name: "fuchsia rose",
+        year: 2001,
+        color: "#C74375",
+        pantone_value: "17-2031",
+    },
+};
+
+const productsData = {
+    page: 1,
+    per_page: 5,
+    total: 12,
+    total_pages: 3,
+    data: [
+        {
+            id: 1,
+            name: "cerulean",
+            year: 2000,
+            color: "#98B2D1",
+            pantone_value: "15-4020",
+        },
+        {
+            id: 2,
+            name: "fuchsia rose",
+            year: 2001,
+            color: "#C74375",
+            pantone_value: "17-2031",
+        },
+    ],
+};
+
 beforeEach(() => {
-    fetchMock.resetMocks();
+    jest.restoreAllMocks();
 });
 
 describe("App component", () => {
     it("should render error modal", async () => {
-        fetchMock.mockResponseOnce(JSON.stringify({}), { status: 404 });
+        mockFetch(false, 404, {});
         render(<MockApp path={`/?id=100`} />);
         const errorModal = await screen.findByText("Error");
         expect(errorModal).toBeVisible();
     });
+
     it("should render filtered product", async () => {
-        fetchMock.mockResponseOnce(
-            JSON.stringify({
-                data: {
-                    id: 2,
-                    name: "fuchsia rose",
-                    year: 2001,
-                    color: "#C74375",
-                    pantone_value: "17-2031",
-                },
-            }),
-            { status: 200 }
-        );
+        mockFetch(true, 200, productData);
         render(<MockApp path={`/?id=2`} />);
         const product = await screen.findByText("fuchsia rose");
         expect(product).toBeVisible();
     });
 
     it("should render list with products data", async () => {
-        fetchMock.mockResponseOnce(
-            JSON.stringify({
-                page: 1,
-                per_page: 5,
-                total: 12,
-                total_pages: 3,
-                data: [
-                    {
-                        id: 1,
-                        name: "cerulean",
-                        year: 2000,
-                        color: "#98B2D1",
-                        pantone_value: "15-4020",
-                    },
-                    {
-                        id: 2,
-                        name: "fuchsia rose",
-                        year: 2001,
-                        color: "#C74375",
-                        pantone_value: "17-2031",
-                    },
-                ],
-            }),
-            { status: 200 }
-        );
+        mockFetch(true, 200, productsData);
         render(<MockApp path={"/"} />);
         const product = await screen.findByText("fuchsia rose");
         expect(product).toBeVisible();
